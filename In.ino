9985f602e84689b1cd56e5e04511e604658e8f68
Eittetev <57396000+Eittetev@users.noreply.github.com>
2019-11-05 19:32:27 +0300

    Add files via upload

---
+++ In.ino
@@ -0,0 +1,79 @@
+#include <VirtualWire.h>
+  byte message[VW_MAX_MESSAGE_LEN]; // Буфер для хранения принимаемых данных
+  byte messageLength = VW_MAX_MESSAGE_LEN; // Размер сообщения
+  const int receiver_pin = 12; // Пин подключения приемника
+void setup()
+{
+  Serial.begin(9600); // Скорость передачиданных
+  Serial.println("MX-RM-5V is ready");
+  vw_set_rx_pin(receiver_pin); // Пин подключения приемника
+  vw_setup(2000); // Скорость передачи данных (бит в секунду)
+  vw_rx_start(); // Активация применика
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(11, OUTPUT);
+  digitalWrite(7,LOW);
+}
+void loop()
+{
+  int thr,ster,ster2,i; // Переменные управления и скорости
+  ster2 = 512;
+  if (vw_get_message(message, &messageLength)) // Если есть данные..
+  {
+  //Throttle
+  thr = ((message[0]) << 8) + ((message[1])); // Перевод в десятичное 2 байт
+  if (byte(message[0])==5) // Если 0 байт
+  {thr = 0;}
+  if (byte(message[0])==4) // Если 1 байт
+  {thr = ((message[1]));}
+  //Steering
+  ster = ((message[2]) << 8) + ((message[3])); // Перевод в десятичное 2 байт
+  if (byte(message[2])==5)  // Если 0 байт
+  {ster = 0;}
+  if (byte(message[2])==4)  // Если 1 байт
+  {ster = ((message[3]));}
+  analogWrite(11,thr/4);
+  // Вывод на экран
+  Serial.println();
+  Serial.print("Throttle = ");
+  Serial.print(thr);
+  Serial.println();
+  Serial.print("Steering = ");
+    if (ster < 512)
+  { Serial.print("Right ");
+    Serial.print(512-ster);
+  }
+     if (ster > 512)
+  { Serial.print("Left ");
+    Serial.print(ster-512);
+  }
+  Serial.println();
+
+  }
+  ster2 = ster2 - ster;
+  if (ster > 512)
+  {
+    ster2 = ster-512;
+    digitalWrite(6,LOW);
+    for (i = 512;i<ster2;i++)
+    {
+      digitalWrite(5,HIGH);
+      delay(1);
+      digitalWrite(5,LOW);
+    }
+  }
+  if (ster < 512)
+  {
+    ster2 = 512-ster;
+    digitalWrite(6,HIGH);
+    for (i = 0;i<ster2;i++)
+    {
+      digitalWrite(5,HIGH);
+      delay(1);
+      digitalWrite(5,LOW);
+    }
+    digitalWrite(6,LOW);
+  }
+
+}
---
+++ out.ino
@@ -0,0 +1,59 @@
+#include <VirtualWire.h>
+
+const int led_pin = 13; // Пин светодиода
+const int transmit_pin = 12; // Пин подключения передатчика
+const int thr = A0; //Пин акк
+const int steer = A1; //Пин управления
+
+void setup()
+{
+  vw_set_tx_pin(transmit_pin);
+  vw_setup(2000);       // Скорость передачи (Бит в секунду)
+  Serial.begin(9600);
+  Serial.println("Engine powering up");
+  pinMode(led_pin, OUTPUT);
+  pinMode(transmit_pin, OUTPUT);
+  pinMode(thr, INPUT);
+  pinMode(steer, INPUT);
+}
+
+void loop()
+{
+
+  int thrt,ster;
+  char msg[4];
+  thrt = analogRead(thr); // Чтение скорости
+  ster = analogRead(steer); // Чтение поворота
+  Serial.println(ster);
+  //Throttle
+  if (thrt > 255) { // Разбить на байты
+  msg[1] = byte(thrt);
+  msg[0] = byte(thrt >> 8);
+  }
+  else { //Назначить код если только младший байт
+  msg[1] = byte(thrt);
+  msg[0] = 4;   
+  } //Назначить код если 0 байт
+  if (thrt == 0) {
+  msg[1] = 1;
+  msg[0] = 5;
+  }
+  //Steering
+  if (ster > 255) { // Разбить на байты
+  msg[3] = byte(ster);
+  msg[2] = byte(ster >> 8);
+  }
+  else { //Назначить код если только младший байт
+  msg[3] = byte(ster);
+  msg[2] = 4;   
+  } //Назначить код если 0 байт
+  if (ster == 0) {
+  msg[3] = 1;
+  msg[2] = 5;
+  }
+  digitalWrite(led_pin, HIGH); // Зажигаем светодиод в начале передачи
+  vw_send((uint8_t *)msg, strlen(msg)); // Отправка сообщения
+  vw_wait_tx(); // Ожидаем окончания отправки сообщения
+  digitalWrite(led_pin, LOW); // Гасим светодиод в конце передачи
+  delay(500); // Пауза 0.5 секунды
+}
